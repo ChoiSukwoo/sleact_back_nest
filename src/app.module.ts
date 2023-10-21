3065820261cfeab53685a2c704ea0d24d80cb090
Niena <58356541+ChoiSukwoo@users.noreply.github.com>
2023-10-22 01:38:25 +0900

    미들웨어 적용 예제
    feat : 미들웨어 적용 예제 진행

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
-export class AppModule {}
+export default class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,23 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('Http');
+
+  use(req: Request, res: Response, next: NextFunction): void {
+    const { ip, method, originalUrl } = req;
+    const userAgent = req.get('user-agent');
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const contentLength = res.get('contentLength');
+
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+      );
+    });
+
+    next();
+  }
+}
