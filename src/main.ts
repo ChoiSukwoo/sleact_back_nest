9b9e22b551ab684c4e42a050fc705da6b6808bb8
Niena <58356541+ChoiSukwoo@users.noreply.github.com>
2023-10-22 00:22:45 +0900

    Hot Reload 기능 추가
    feat : Hot Reload 기능 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
+  await app.listen(port);
+
+  console.log(`listening on port ${port}`);
 }
+
 bootstrap();
